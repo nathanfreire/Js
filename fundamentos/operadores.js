93ecbfa9e9833305c06e7fbf9d9fdd635589bfd2
Nathan Freire <nathan.freire1403@gmail.com>
2025-01-22 17:14:22 -0300

    5 funções

---
+++ fundamentos/operadores.js
@@ -0,0 +1,45 @@
+/**
+ * OPERADORES
+ * - Aritméticos
+ * - Aritméticos de atribuição
+ * - Comparação
+ * - Lógicos
+ */
+
+console.clear()
+//
+console.log("Operadores aritméticos")
+console.log("+ | soma")
+console.log("- | subtração")
+console.log("* | multiplicação")
+console.log("/ | divisão")
+console.log("% | módulo (resto da divisão)")
+console.log("")
+//
+console.log("Operadores aritméticos de atribuição")
+console.log("+= | soma e atribuição variável")
+console.log("-= | subtração e atribuição variável")
+console.log("*= | multiplicação e atribuição variável")
+console.log("/= | divisão e atribuição variável")
+console.log("++ | soma 1 ao resultado")
+console.log("-- | subtração 1 do resultado")
+console.log("")
+//
+console.log("Comparadores")
+console.log("> | miaor que")
+console.log("< | menor que")
+console.log(">= | maior ou igual a")
+console.log("<= | menor ou igual a")
+console.log("=== | identico a")
+console.log("!== | não identico a")
+console.log("")
+//
+// lembrar o professor do contra passo
+console.log("Lógicos")
+console.log(" | ")
+console.log(" | ")
+console.log(" | ")
+console.log(" | ")
+console.log(" | ")
+console.log(" | ")
+console.log("")
